2f33cea2877fe5d582bbf11650185255d3350444
Kausik Samanta <kausik.smnt@gmail.com>
2018-12-18 06:45:18 +0530

    first commit

---
+++ src/main/java/com/test/SampleTestRest.java
@@ -0,0 +1,20 @@
+package com.test;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("/restservices")
+public class SampleTestRest {
+
+
+	@GET
+	@Path("/get")
+	@Produces(MediaType.TEXT_PLAIN)
+	public String getTestService() {
+		return "Hello World! This is coming from webservice!!";
+	}
+
+
+}
